87fb051b7e7bdaea4f0530fa600846e0cb7aa8a2
light-co-turkey <light.co.turkey@gmail.com>
2023-07-18 12:15:24 +0300

    def-conf-temp

--- nginx.sh
+++ nginx.sh
@@ -22,17 +22,19 @@ if [[ "$TEST_MODE" != "true" ]]; then
     # Get the first domain from the DOMAINS environment variable
     DOMAIN=$(echo $DOMAINS | awk '{print $1}')
 
-    # Create the server_name_switch placeholder using all domains separated by "|"
-    SERVER_NAME_SWITCH=$(echo $DOMAINS | tr ' ' '|')
+    # Create a temporary copy of def-server-conf for modifications
+    cp def-server-conf def-server-conf-temp
 
-    # Replace the placeholders in the def-server-conf.txt file with the corresponding values from .env
-    sed -i "s/server_name_placeholder/$DOMAINS/g" def-server-conf
-    sed -i "s/first_domain_placeholder/$DOMAIN/g" def-server-conf
-    sed -i "s/server_name_switch_placeholder/$SERVER_NAME_SWITCH/g" def-server-conf
-    sed -i "s/proxy_pass_placeholder/http:\/\/localhost:$DEPLOY_PORT/g" def-server-conf
+    # Replace the placeholders in the temporary file with the corresponding values from .env
+    sed -i "s/server_name_placeholder/$DOMAINS/g" def-server-conf-temp
+    sed -i "s/first_domain_placeholder/$DOMAIN/g" def-server-conf-temp
+    sed -i "s/proxy_pass_placeholder/http:\/\/localhost:$DEPLOY_PORT/g" def-server-conf-temp
 
-    # Move the updated configuration file to the Nginx sites-available directory
-    sudo cp def-server-conf $DEFAULT_SERVER_CONF
+    # Move the modified temporary file to the Nginx sites-available directory
+    sudo cp def-server-conf-temp $DEFAULT_SERVER_CONF
+
+    # Remove the temporary file
+    rm def-server-conf-temp
 
     # Restart Nginx to apply the changes
     sudo systemctl restart nginx
